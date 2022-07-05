dc76f849351417fbdb55b9cbb4b1c109c38f8b27
dowdc <Cian.Dowd@infineon.com>
2022-07-05 16:32:38 +0100

    updates to mouse toggles ahead of control mouse 2

--- toggles.py
+++ toggles.py
@@ -1,6 +1,5 @@
 from talon import Context, Module, actions, settings, ui, ctrl
 from talon_plugins import eye_mouse, eye_zoom_mouse
-from talon_plugins.eye_mouse import config, toggle_control
 
 mod = Module()
 
@@ -12,27 +11,27 @@ class Actions:
         """Toggles the most recent mouse mode"""
         global last_active
         # if not using zoom mouse, see if we meet the criteria for using control mouse
-        if (config.control_mouse or last_active == "control") and not eye_zoom_mouse.zoom_mouse.enabled:
+        if (eye_mouse.config.control_mouse or last_active == "control") and not eye_zoom_mouse.zoom_mouse.enabled:
             print("toggling control mouse")
             last_active = "control" # remember that we used control mouse recently
-            toggle_control(not config.control_mouse)
+            actions.tracking.control_toggle(not eye_mouse.config.control_mouse)
         # default to using zoom mouse
         else:
             print("toggling zoom mouse")
             last_active = "zoom" # remember that we used zoom mouse recently
-            eye_zoom_mouse.toggle_zoom_mouse(not eye_zoom_mouse.zoom_mouse.enabled)
+            actions.tracking.control_zoom_toggle(not eye_zoom_mouse.zoom_mouse.enabled)
 
     def switch_mouse():
         """Switches between the two mouse modes"""
-        if not config.control_mouse:
-            eye_zoom_mouse.toggle_zoom_mouse(False)
+        if not eye_mouse.config.control_mouse:
+            actions.tracking.control_zoom_toggle(False)
             print("disabling zoom mouse")
-            toggle_control(True)
+            actions.tracking.control_toggle(True)
             print("enabling control mouse")
         else:
-            toggle_control(False)
+            actions.tracking.control_toggle(False)
             print("disabling control mouse")
-            eye_zoom_mouse.toggle_zoom_mouse(True)
+            actions.tracking.control_zoom_toggle(True)
             print("enabling zoom mouse")
 
 
